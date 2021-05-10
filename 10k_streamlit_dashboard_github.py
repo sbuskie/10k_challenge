799ed3d443ad36c553ac4eff94e09325ac6eca35
sbuskie <stephenbuskie2@hotmail.com>
2021-05-10 18:07:59 -0500

    commented out sleep for video

--- 10k_streamlit_dashboard_github.py
+++ 10k_streamlit_dashboard_github.py
@@ -366,8 +366,8 @@ st.subheader("This isn't a race, but if it was, it would probably be the best ra
 
 
 with st.spinner(text='video loading, please remain calm'):
-	time.sleep(15)
-st.success('almost there... almost there... almost there...')
+#	time.sleep(15)
+#st.success('almost there... almost there... almost there...')
 
 #TODO - commented out video creation since streamlit deployment on github does not have ffmpeg video codec.
 
